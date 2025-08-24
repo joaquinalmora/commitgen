b89e451acaed249e4750783d123811ec98f39211
Joaquin Almora <jalmora@student.ubc.ca>
2025-08-24 00:13:34 -0700

    added hook

--- cmd/commitgen/main.go
+++ cmd/commitgen/main.go
@@ -4,23 +4,64 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/joaquinalmora/commitgen/internal/diff"
 	"github.com/joaquinalmora/commitgen/internal/prompt"
 )
 
-func main() {
-	if len(os.Args) > 1 {
-		cliArg := os.Args[1]
+type Command struct {
+	Description string
+	Run         func(args []string)
+}
 
-		if cliArg == "suggest" {
+var commands = map[string]Command{
+	"suggest": {
+		Description: "Suggest a commit message based on staged changes",
+		Run: func(args []string) {
 			suggest()
-		} else {
-			fmt.Println("Unknown command: " + cliArg)
-		}
-	} else {
-		fmt.Println("Usage: commitgen <command>\nAvailable commands:\n  suggest")
+		},
+	},
+	"install-hook": {
+		Description: "Install a git commit hook to auto-suggest commit messages",
+		Run: func(args []string) {
+			installHook()
+		},
+	},
+}
+
+func printUsage(commands map[string]Command) {
+	fmt.Println("Usage: commitgen <command> [options]")
+	fmt.Println("Available commands:")
+
+	keys := make([]string, 0, len(commands))
+	for k := range commands {
+		keys = append(keys, k)
 	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("  %-13s %s\n", k, commands[k].Description)
+	}
+
+}
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		printUsage(commands)
+		return
+	}
+
+	name := os.Args[1]
+
+	cmd, ok := commands[name]
+
+	if !ok {
+		fmt.Println("Unknown command: %s\n\n", name)
+		printUsage(commands)
+		return
+	}
+
+	cmd.Run(os.Args[2:])
 }
 
 func suggest() {
--- internal/hook/hook.go
+++ internal/hook/hook.go
@@ -1,3 +1,9 @@
 package hook
 
+import "fmt"
+
 // Installs git hooks that call binary to prefill messages
+
+func installHook() {
+	fmt.Println("hook installed")
+}
